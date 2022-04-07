f395949db150a5dacad7f3277236752a9cdc6b66
Faddey Kryachko <faddeyca@gmail.com>
2022-04-07 22:14:37 +0500

    battle 02

--- Assets/_Scripts/BattleScripts/BattleManager.cs
+++ Assets/_Scripts/BattleScripts/BattleManager.cs
@@ -6,13 +6,51 @@ public class BattleManager: MonoBehaviour
 {
     public static BattleManager self;
     public Unit prefab;
-
+    public Mover mover { get; private set; }
     private List<Tuple<Unit, UnitInfo>> units;
 
     void Awake()
     {
         self = this;
         units = new List<Tuple<Unit, UnitInfo>>();
+        mover = Mover.start;
+    }
+
+    void Update()
+    {
+        if (mover == Mover.start)
+        {
+            mover = Mover.player;
+            Debug.Log("Start -> Player move");
+            return;
+        }
+        if (mover == Mover.enemy)
+        {
+            mover = Mover.player;
+            Debug.Log("Enemy -> Player move");
+            return;
+        }
+    }
+
+    public void StopPlayerMove()
+    {
+        if (mover != Mover.player)
+        {
+            Debug.LogError("Not player move");
+            return;
+        }
+        mover = Mover.enemy;
+    }
+
+    public void StopEnemyMove()
+    {
+        if (mover != Mover.enemy)
+        {
+            Debug.LogError("Not enemy move");
+            return;
+        }
+
+        mover = Mover.player;
     }
 
     void Start()
@@ -34,4 +72,11 @@ public class BattleManager: MonoBehaviour
             units.Add(Tuple.Create(obj, info));
         }
     }
+}
+
+public enum Mover
+{
+    enemy = -1,
+    start = 0,
+    player = 1
 }

1fa1a5d01be0b0136259ce203654034d16afab16
Faddey Kryachko <faddeyca@gmail.com>
2022-04-09 17:05:01 +0500

    first version:

---
+++ Assets/_Scripts/MmScripts/HeroMotionManager.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class HeroMotionManager : MonoBehaviour
+{
+    public const float speed = 0.05f;
+
+    public Vector2Int direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal") * speed;
+        var vertical = Input.GetAxis("Vertical") * speed;
+        var v = new Vector3(horizontal, vertical, 0);
+        SetDirection(horizontal, vertical);
+        var x = (int) Math.Round(transform.position.x);
+        var y = (int) Math.Round(transform.position.y);
+        if (MapObjectManager.instance[x + direction.x, y + direction.y] == null)
+        {
+            transform.position += v;
+        }
+
+    }
+
+    private void SetDirection(float horizontal, float vertical)
+    {
+        if (horizontal > 0)
+        {
+            if (vertical == 0)
+            {
+                direction.y = 0;
+            }
+            direction.x = 1;
+        }
+        if (horizontal < 0)
+        {
+            if (vertical == 0)
+            {
+                direction.y = 0;
+            }
+            direction.x = -1;
+        }
+        if (vertical > 0)
+        {
+            if (horizontal == 0)
+            {
+                direction.x = 0;
+            }
+            direction.y = 1;
+        }
+        if (vertical < 0)
+        {
+            if (horizontal == 0)
+            {
+                direction.x = 0;
+            }
+            direction.y = -1;
+        }
+    }
+}

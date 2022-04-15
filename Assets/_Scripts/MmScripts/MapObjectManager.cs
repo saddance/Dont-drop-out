5dac169cb27b23bd8fd9ede90fcba13a546c745c
Catmoonlight <catmoonlight53@gmail.com>
2022-04-15 09:58:33 +0500

    Tab changed

--- Assets/_Scripts/MmScripts/MapObjectManager.cs
+++ Assets/_Scripts/MmScripts/MapObjectManager.cs
@@ -1,55 +1,55 @@
-    using System.Collections;
-    using System.Collections.Generic;
-    using UnityEditor;
-    using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
 
-    public class MapObjectManager : MonoBehaviour
-    {
-        public static MapObjectManager instance = null;
-        private GameObject[,] gridArray;
-        
-        public GameObject PrefabObstacle;
+public class MapObjectManager : MonoBehaviour
+{
+	public static MapObjectManager instance = null;
+	private GameObject[,] gridArray;
+	
+	public GameObject PrefabObstacle;
 
-        public GameObject this[int x, int y]
-        {
-            get { return gridArray[x, y]; }
-            set
-            {
-                if (value != null && gridArray[x, y] != null)
-                    Debug.LogError("InvalidIndexes");
-                else
-                    gridArray[x, y] = value;
-            }
-        }
+	public GameObject this[int x, int y]
+	{
+		get { return gridArray[x, y]; }
+		set
+		{
+			if (value != null && gridArray[x, y] != null)
+				Debug.LogError("InvalidIndexes");
+			else
+				gridArray[x, y] = value;
+		}
+	}
 
-        public void MakeField(int height, int width)
-        {
-            gridArray = new GameObject[height, width];
-        }
+	public void MakeField(int height, int width)
+	{
+		gridArray = new GameObject[height, width];
+	}
 
-        void Awake()
-        {
-            if (instance == null)
-                instance = this;
-        }
+	void Awake()
+	{
+		if (instance == null)
+			instance = this;
+	}
 
-        public void GenerateByPrefab(GameObject prefab, int x, int y)
-        {
-            // Instatiate от предка, мб, можно без него
-            if (prefab == null)
-                return;
-            var obj = Instantiate(prefab, transform);
-            obj.transform.position = new Vector3(x, y, 0);
-            this[x, y] = Instantiate(prefab);
-        }
+	public void GenerateByPrefab(GameObject prefab, int x, int y)
+	{
+		// Instatiate от предка, мб, можно без него
+		if (prefab == null)
+			return;
+		var obj = Instantiate(prefab, transform);
+		obj.transform.position = new Vector3(x, y, 0);
+		this[x, y] = Instantiate(prefab);
+	}
 
-        void Start()
-        {
+	void Start()
+	{
 
-        }
+	}
 
-        void Update()
-        {
+	void Update()
+	{
 
-        }
-    }
+	}
+}

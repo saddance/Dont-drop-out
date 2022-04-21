1f4b8e160175b473f2db8abf2b2d8156b6a40105
Catmoonlight <catmoonlight53@gmail.com>
2022-04-21 21:23:37 +0500

    Renew moving system

---
+++ Assets/_Scripts/MmScripts/Helpers/WASDHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASDHandler
+{
+    static KeyCode[] handlingCodes = new[] { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D };
+
+    private List<Tuple<KeyCode, float>> pressedButtons = new List<Tuple<KeyCode, float>>();
+    
+    public KeyCode? GetPressedButton()
+    {
+        foreach(var code in handlingCodes)
+        {
+            if (Input.GetKey(code))
+            {
+                if (pressedButtons.Count(x => x.Item1 == code) == 0)
+                    pressedButtons.Add(Tuple.Create(code, Time.time));
+            }
+            else
+                pressedButtons = pressedButtons.Where(x => x.Item1 != code).ToList();
+        }
+        if (pressedButtons.Count == 0)
+            return null;
+        return pressedButtons.OrderBy(x => x.Item2).Last().Item1;
+        
+    }
+}
+
---
+++ Assets/_Scripts/MmScripts/HeroMotion.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEditor;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+using System.Threading;
+
+public class HeroMotion : MonoBehaviour
+{
+    [Header("Walking constants")]
+    [SerializeField] private float moveTime;
+    [SerializeField] private float comeBackTime;
+
+    public Vector3Int lastDirection { get; private set; }
+    private bool isMoving = false;
+    private WASDHandler handler = new WASDHandler();
+
+    void Update()
+    {
+
+        if (!isMoving)
+        {
+
+            // TODO : rewrite this shit
+            var code = handler.GetPressedButton();
+            if (code == null)
+                return;
+            else if (code == KeyCode.W)
+                TryGo(new Vector3Int(0, 1, 0), code.Value);
+            else if (code == KeyCode.A)
+                TryGo(new Vector3Int(-1, 0, 0), code.Value);
+            else if (code == KeyCode.S)
+                TryGo(new Vector3Int(0, -1, 0), code.Value);
+            else if (code == KeyCode.D)
+                TryGo(new Vector3Int(1, 0, 0), code.Value);
+        }
+    }
+
+    void TryGo(Vector3Int direction, KeyCode code)
+    {
+        lastDirection = direction;
+        if (CanIGo())
+        {
+            isMoving = true;
+            StartCoroutine(Walk(direction, code));
+        }
+    }
+
+    IEnumerator Walk(Vector3 direction, KeyCode code)
+    {
+        float startTime = Time.time;
+        Vector3 startPosition = transform.position;
+
+        while (Time.time <= startTime + moveTime)
+        {
+            float elapsed = Time.time - startTime;
+            if (elapsed <= comeBackTime && handler.GetPressedButton() != code)
+            {
+                direction = Vector3.zero;
+                break;
+            }
+            transform.position = startPosition + (elapsed / moveTime) * direction;
+            yield return new WaitForEndOfFrame();
+        }
+
+        transform.position = startPosition + direction;
+        isMoving = false;
+        yield break;
+    }
+
+    bool CanIGo()
+    {
+        try
+        {
+            return MapObjectManager.instance[Mathf.RoundToInt(transform.position.x) + lastDirection.x,
+                Mathf.RoundToInt(transform.position.y) + lastDirection.y] == null;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+}

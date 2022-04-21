74c5fde07f3d2241b6c20370dc931c1a381ab1cf
fadg25 <fadg25@yandex.ru>
2022-04-21 20:10:44 +0500

    refactoring

--- Assets/_Scripts/MmScripts/HeroMotionManager.cs
+++ Assets/_Scripts/MmScripts/HeroMotionManager.cs
@@ -5,114 +5,105 @@ using System.Numerics;
 using UnityEditor;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
+using System.Threading;
 
 public class HeroMotionManager : MonoBehaviour
 {
-    public float speed = 0.05f;
+    public Vector3Int direction;
 
-    public Vector2Int direction;
-    // Start is called before the first frame update
-    void Start()
-    {
-        transform.gameObject.tag = "Player";
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        var horizontal = Input.GetAxis("Horizontal") * speed;
-        var vertical = Input.GetAxis("Vertical") * speed;
-        var v = new Vector3(horizontal, vertical, 0);
-        SetDirection(horizontal, vertical);
-        if (v == Vector3.zero)
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown("w"))
         {
-            v.x = (float) (Math.Round(transform.position.x) - transform.position.x);
-            v.y = (float) (Math.Round(transform.position.y) - transform.position.y);
-        }
-        if (CanIGo())
-        {
-            transform.position += v;
-        }
-    }
-
-    private bool CanIGo()
-    {
-        if (direction.x < 0 && direction.y == 0)
-        {
-            return MapObjectManager.instance[(int) Math.Ceiling(transform.position.x) - 1,
-                (int) Math.Round(transform.position.y)] == null;
-        }
-        if (direction.x > 0 && direction.y == 0)
-        {
-            return MapObjectManager.instance[(int) transform.position.x + 1,
-                (int) Math.Round(transform.position.y)] == null;
-        }
-        if (direction.y < 0 && direction.x == 0)
-        {
-            return MapObjectManager.instance[(int) Math.Round(transform.position.x),
-                (int) Math.Ceiling(transform.position.y) - 1] == null;
-        }
-        if (direction.y > 0 && direction.x == 0)
-        {
-            return MapObjectManager.instance[(int) Math.Round(transform.position.x),
-                (int) transform.position.y + 1] == null;
-        }
-        if (direction.x < 0 && direction.y < 0)
-        {
-            return MapObjectManager.instance[(int) Math.Ceiling(transform.position.x) - 1,
-                (int) Math.Ceiling(transform.position.y) - 1] == null;
-        }
-        if (direction.x < 0 && direction.y > 0)
-        {
-            return MapObjectManager.instance[(int) Math.Ceiling(transform.position.x) - 1,
-                (int) transform.position.y + 1] == null;
-        }
-        if (direction.x > 0 && direction.y < 0)
-        {
-            return MapObjectManager.instance[(int) transform.position.x + 1,
-                (int) Math.Ceiling(transform.position.y) - 1] == null;
-        }
-        if (direction.x > 0 && direction.y > 0)
-        {
-            return MapObjectManager.instance[(int) transform.position.x + 1,
-                (int) transform.position.y + 1] == null;
-        }
-        return true;
-    }
-
-    private void SetDirection(float horizontal, float vertical)
-    {
-        if (horizontal > 0)
-        {
-            if (vertical == 0)
-            {
-                direction.y = 0;
-            }
-            direction.x = 1;
-        }
-        if (horizontal < 0)
-        {
-            if (vertical == 0)
-            {
-                direction.y = 0;
-            }
-            direction.x = -1;
-        }
-        if (vertical > 0)
-        {
-            if (horizontal == 0)
-            {
-                direction.x = 0;
-            }
+            direction.x = 0;
             direction.y = 1;
-        }
-        if (vertical < 0)
-        {
-            if (horizontal == 0)
+            if (CanIGo())
             {
-                direction.x = 0;
+                Moving();
             }
-            direction.y = -1;
         }
+        if (vertical == 1)
+        {
+            direction.x = 0;
+            direction.y = 1;
+            if (CanIGo())
+            {
+                Running();
+            }
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            direction.x = 0;
+            direction.y = -1;
+            if (CanIGo())
+            {
+                Moving();
+            }
+        }
+        if (vertical == -1)
+        {
+            direction.x = 0;
+            direction.y = -1;
+            if (CanIGo())
+            {
+                Running();
+            }
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            direction.x = 1;
+            direction.y = 0;
+            if (CanIGo())
+            {
+                Moving();
+            }
+        }
+        if (horizontal == 1)
+        {
+            direction.x = 1;
+            direction.y = 0;
+            if (CanIGo())
+            {
+                Running();
+            }
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            direction.x = -1;
+            direction.y = 0;
+            if (CanIGo())
+            {
+                Moving();
+            }
+        }
+        if (horizontal == -1)
+        {
+            direction.x = -1;
+            direction.y = 0;
+            if (CanIGo())
+            {
+                Running();
+            }
+        }
+    }
+
+    bool CanIGo()
+    {
+        return MapObjectManager.instance[(int) transform.position.x + direction.x,
+            (int) transform.position.y + direction.y] == null;
+    }
+
+    void Moving()
+    {
+        transform.position += direction;
+        Thread.Sleep(160);
+    }
+    
+    void Running()
+    {
+        transform.position += direction;
+        Thread.Sleep(100);
     }
 }

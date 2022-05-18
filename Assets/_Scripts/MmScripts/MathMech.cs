282cdee3bbb217e53513bf263d083a8c4f116635
kolya <saddance7@gmail.com>
2022-05-18 09:10:34 +0500

    1

--- Assets/_Scripts/MmScripts/MathMech.cs
+++ Assets/_Scripts/MmScripts/MathMech.cs
@@ -10,20 +10,17 @@ using Random = UnityEngine.Random;
 
 namespace _Scripts.MmScripts
 {
+    // AsMapObject пока не используется 
     [Serializable]
     public class AsMapObject
     {
         public List<int> Labels;
-        AsMapObject()
-        {
-            Labels = Enumerable.Range(0, 15).ToList();
-        }
     }
     public class MathMech : MonoBehaviour
     {
         
     [SerializeField] private TextAsset fieldText;
-    [SerializeField] private GameObject prefabObstacle;
+    //[SerializeField] private GameObject prefabObstacle;
     [SerializeField] private GameObject prefabInteractable;
 
     private void Start()
@@ -40,19 +37,21 @@ namespace _Scripts.MmScripts
         {
             if (save.personalities[i].hidden)
                 continue;
-
+            
+            // Строчка ниже не подходит под то что спрайты разные у нас 
+            // Отрефактори, Витя ^_^
             MapObjectManager.instance.GenerateByPrefab(
                 prefabInteractable,
                 save.mapPositions[i].x,
                 save.mapPositions[i].y);
-
+            
             MapObjectManager.instance[save.mapPositions[i].x, save.mapPositions[i].y]
                 .GetComponent<InteractableObject>()
                 .Init(save.personalities[i]);
         }
     }
 
-    private void CheckForPositionInit(List<Vector2Int> spawnPositions)
+    private void CheckForPositionInit(List<List<Vector2Int>> spawnPositions)
     {
         var save = GameManager.currentSave;
 
@@ -63,13 +62,13 @@ namespace _Scripts.MmScripts
             {
                 if (save.personalities[i].hidden)
                     continue;
-                save.mapPositions[i] = spawnPositions[Random.Range(0, spawnPositions.Count)];
-                spawnPositions.Remove(save.mapPositions[i].GetV());
+                var type = Random.Range(0, 16);
+                save.mapPositions[i] = spawnPositions[type][Random.Range(0, spawnPositions[type].Count)];
+                spawnPositions[type].Remove(save.mapPositions[i].GetV());
             }
         }
     }
-
-    private List<Vector2Int> ProcessFileAndSpawnObst()
+    private List<List<Vector2Int>> ProcessFileAndSpawnObst()
     {
         var lines = fieldText.text.Split('\n');
 
@@ -77,16 +76,18 @@ namespace _Scripts.MmScripts
         var width = lines[0].Trim().Length;
 
         MapObjectManager.instance.MakeField(width, height);
-        var spawnPositions = new List<Vector2Int>();
+        var spawnPositions = new List<List<Vector2Int>>(16);
 
         for (var i = 0; i < height; i++)
         for (var j = 0; j < width; j += 3)
         {
             // X00 - XFF and SpaceSpaceSpace
             var hexNumber = lines[i].Substring(j, 3);
-            if (hexNumber[0] != ' ')
+            var firstSymbol = hexNumber[0];
+            if (firstSymbol != ' ')
             {
-                spawnPositions.Add(new Vector2Int(j / 3, i));
+                var index = Char.IsDigit(firstSymbol) ? firstSymbol - '0' : firstSymbol - 'A';
+                spawnPositions[index].Add(new Vector2Int(j / 3, i));
                 var pathToFile = "Sprites/sprite" + hexNumber.Substring(1, 2);
                 var sprite = Resources.Load<Sprite>(pathToFile);
                 var gameObj = new GameObject();
@@ -96,17 +97,14 @@ namespace _Scripts.MmScripts
                 gameObj.transform.position = new Vector3(j / 3, i, 0);
                 if (hexNumber[1] <= '7')
                 {
-                    //
                 }
 
                 if (hexNumber[1] >= '8')
                 {
                     MapObjectManager.instance[j / 3, i] = gameObj;
                 }
-
             }
         }
-
         return spawnPositions;
     }
     }

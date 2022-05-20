31c5ad433eba88fd7d3afe0a9e816f2910064df1
kolya <saddance7@gmail.com>
2022-05-20 15:02:24 +0500

    Changes were made:

--- Assets/_Scripts/MmScripts/MathMech.cs
+++ Assets/_Scripts/MmScripts/MathMech.cs
@@ -20,7 +20,8 @@ namespace _Scripts.MmScripts
     public class MathMech : MonoBehaviour
     {
         [SerializeField] private TextAsset fieldText;
-        [SerializeField] private GameObject prefabObstacle;
+
+        //[SerializeField] private GameObject prefabObstacle;
         [SerializeField] private GameObject prefabInteractable;
 
         private void Start()
@@ -39,7 +40,7 @@ namespace _Scripts.MmScripts
                     continue;
 
                 // Строчка ниже не подходит под то что спрайты разные у нас 
-                // Отрефактори, Витя ^_^
+                // Либо если роль декора просто покрывать место где стоят спрайты то всё норм
                 MapObjectManager.instance.GenerateByPrefab(
                     prefabInteractable,
                     save.mapPositions[i].x,
@@ -60,10 +61,11 @@ namespace _Scripts.MmScripts
                 save.mapPositions = new Vector2IntS[save.personalities.Length];
                 for (var i = 0; i < save.mapPositions.Length; i++)
                 {
+                    var notEmptySpawnPositions = spawnPositions.Where(arr => arr.Count != 0).ToList();
                     if (save.personalities[i].hidden)
                         continue;
-                    var type = Random.Range(0, 16);
-                    save.mapPositions[i] = spawnPositions[type][Random.Range(0, spawnPositions[type].Count)];
+                    var type = Random.Range(0, notEmptySpawnPositions.Count);
+                    save.mapPositions[i] = notEmptySpawnPositions[type][Random.Range(0, notEmptySpawnPositions[type].Count)];
                     spawnPositions[type].Remove(save.mapPositions[i].GetV());
                 }
             }
@@ -91,27 +93,24 @@ namespace _Scripts.MmScripts
                 {
                     var index = Char.IsDigit(firstSymbol) ? firstSymbol - '0' : 10 + firstSymbol - 'A';
                     spawnPositions[index].Add(new Vector2Int(j / 3, i));
-                    var pathToFile = "Sprites/" + hexNumber.Substring(1, 2);
-                    var sprite = Resources.Load<Sprite>(pathToFile);
-                    Debug.Log("Found sprite");
+                }
+                var pathToFile = "Sprites/" + hexNumber.Substring(1, 2);
+                var sprite = Resources.Load<Sprite>(pathToFile);
 
-                    var gameObj = new GameObject();
-                    SpriteRenderer rend = gameObj.AddComponent<SpriteRenderer>();
-                    rend.sprite = sprite;
-                    Instantiate(gameObj);
-                    gameObj.transform.position = new Vector3(j / 3, i, 0);
-                    Debug.Log("Instantiated sprite");
-                    if (hexNumber[1] <= '7')
-                    {
-                    }
+                var gameObj = new GameObject();
+                SpriteRenderer rend = gameObj.AddComponent<SpriteRenderer>();
+                rend.sprite = sprite;
+                Instantiate(gameObj);
+                gameObj.transform.position = new Vector3(j / 3, i, 0);
+                if (hexNumber[1] <= '7')
+                {
+                }
 
-                    if (hexNumber[1] >= '8')
-                    {
-                        MapObjectManager.instance[j / 3, i] = gameObj;
-                    }
+                if (hexNumber[1] >= '8')
+                {
+                    MapObjectManager.instance[j / 3, i] = gameObj;
                 }
             }
-
             return spawnPositions;
         }
     }

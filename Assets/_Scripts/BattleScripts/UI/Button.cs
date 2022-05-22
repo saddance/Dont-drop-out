44a6c6f4c83be4dfd6949c0a3eddfd0609bd8b2c
Faddey Kryachko <faddeyca@gmail.com>
2022-05-22 20:14:26 +0500

    4.1

---
+++ Assets/_Scripts/BattleScripts/UI/Button.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    private PlayerTurnManager ptm;
+    void Start()
+    {
+        ptm = GetComponentInParent<PlayerTurnManager>();
+    }
+    public void Attack()
+    {
+        ptm.TryToAttack();
+    }
+
+    public void Cancel()
+    {
+        ptm.CancelChoice();
+    }
+}
